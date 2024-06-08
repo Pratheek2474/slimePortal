010e16bb1cefc1beb729ac16230dfc2d143bdffc
Pratheek2474 <111521105+Pratheek2474@users.noreply.github.com>
2024-06-08 10:26:26 +0530

    Update st_index.php

--- st_index.php
+++ st_index.php
@@ -1,53 +1,19 @@
-<?php 
-session_start();
+<?php include 'headers.php'; ?>
 
-if(!isset($_SESSION['roll'])){
-    header('Location: login.php');    
-}
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Student: Home</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="images/slime-logo-vector.png" width="150" height="75"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            
-            <a class="nav-link mx-1" aria-current="page" href="#"><h3>Home</h3></a>
-            <a class="nav-link mx-1" aria-current="page" href="profile.php"><h3>Profile</h3></a>
-            <a class="nav-link mx-1" aria-current="page" href="#"><h3>Payment Portal</h3></a>
-            <a class="nav-link mx-1" aria-current="page" href="#"><h3></h3></a>
-        </div>
-        </div>
-    </div>
-    </nav>
-    <main>
-        <div class="container">
-            <div class="row">
+<div class="container">
+            <div class="row m-4 p-5 border border-dark border-1 rounded-5 border-opacity-25 object-fit-none">
+                <h2 class="text-center text-dark p-3">News & Happenings</h2>
                 <div id="news" class="carousel slide" >
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="images/slime-logo-vector.png" class="d-block w-100" alt="...">
+                            <img src="images/1.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/h3.png" class="d-block w-100" alt="...">
+                            <img src="images/2.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                        <img src="images/hi1.png" class="d-block w-100" alt="...">
+                        <img src="images/3.avif" class="d-block w-100" alt="...">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#news" data-bs-slide="prev">
@@ -60,8 +26,47 @@ if(!isset($_SESSION['roll'])){
                     </button>
                     </div>
             </div>
-            <div class="row">
+            <div class="row m-4 p-5 overflow-y: scroll position: absolute">
+                <div class="card">
+                    <h2 class="m-2 p-2 text-center card-title">Notices</h2>
+                    <div class="card-body m-3 p-3">
+                        <div class="card m-3 p-3">
+                            <h5 class="card-title text-danger mb-3 p-2">
+                            Ph.D. Admissions for July, 2024 Session</h5>
+                            <div class="card-body">
+                            Pursuing 7th and 8th semester of B.Tech at IIT Madras, if selected, with an undertaking to do direct PhD at IIT Madras
+                            </div>
+                        </div>
 
+                        <div class="card m-3 p-3">
+                            <h5 class="card-title text-danger mb-3 p-2">
+                            Ph.D. Admissions for July, 2024 Session</h5>
+                            <div class="card-body">
+                            Pursuing 7th and 8th semester of B.Tech at IIT Madras, if selected, with an undertaking to do direct PhD at IIT Madras
+                            </div>
+                        </div>
+
+                        <div class="card m-3 p-3">
+                            <h5 class="card-title text-danger mb-3 p-2">
+                            Ph.D. Admissions for July, 2024 Session</h5>
+                            <div class="card-body">
+                            Pursuing 7th and 8th semester of B.Tech at IIT Madras, if selected, with an undertaking to do direct PhD at IIT Madras
+                            </div>
+                        </div>
+
+                        <div class="card m-3 p-3">
+                            <h5 class="card-title text-danger mb-3 p-2">
+                            Ph.D. Admissions for July, 2024 Session</h5>
+                            <div class="card-body">
+                            Pursuing 7th and 8th semester of B.Tech at IIT Madras, if selected, with an undertaking to do direct PhD at IIT Madras
+                            </div>
+                        </div>
+                        
+                        
+                        
+                    </div>
+                </div>
+                
             </div>
         </div>
     </main>
